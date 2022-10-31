4925eb1c692a36ad1e9facd117af42708572a546
arov05 <113360536+arov05@users.noreply.github.com>
2022-10-31 21:16:16 +0300

    Update solution.js

--- task_1/solution.js
+++ task_1/solution.js
@@ -51,10 +51,13 @@ function addInCartSumChange(sum, delta) {
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     let newSum;
+    newSum = oldSum + difference;
     
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    // замените склеивание, используемое в newSum Text на шаблонную строку
-
+    let newSumText;
+    newSumText = newSum + " ₽";
+    // замените склеивание, используемое в newSum Text на шаблонную строку 
+    newSumText = `newSum ${₽}`;
     // Конец решения задания №1.3.
 
     return newSumText;

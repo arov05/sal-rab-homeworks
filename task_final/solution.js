eef281654ca70fe98f27a17b6b3f68732232b116
arov05 <113360536+arov05@users.noreply.github.com>
2022-11-21 20:27:19 +0300

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,8 +34,15 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+   // let data = {goods: [], order: {}};
+    
+   // function sendRequest(name, phone, address, goods, sum) {
+let data = { client: name + " " + phone, order: { address, sum }, goods: [] };
+//let countOfGoods = goods.length;
+//for (let i = 0; i < countOfGoods; i += 1) {
+//data.goods.push(goods[i].title);
+//}
+    
     let countOfGoods = goods.length;
 
     for (let i = 0; i <= countOfGoods; i += 1) {
